96c55273809d6e4d10e03d6357ccc3b8653ef9e0
ObuleshPolicherla <143755139+ObuleshPolicherla@users.noreply.github.com>
2025-01-30 19:51:48 -0600

    Add files via upload

---
+++ _9_palindromeNumber.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class _9_palindromeNumber {
+	
+	    public boolean isPalindrome(int x) {
+	        String str = Integer.toString(x);
+	        int s =0;
+	        int e = str.length()-1;
+
+	        while(s<e){
+	            if(str.charAt(s)!=str.charAt(e)){
+	                return false;
+	            }
+	            s++;
+	            e--;
+	        }
+
+	        return true;
+	    }
+	
+}

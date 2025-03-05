86be400c1b479aa32f8f0596ee49db752a7f7ee3
ObuleshPolicherla <143755139+ObuleshPolicherla@users.noreply.github.com>
2025-03-05 14:54:48 -0600

    Add files via upload

---
+++ _41_firstMissingPosiitive.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import java.util.Arrays;
+
+class _41_firstMissingPosiitive {
+    public int firstMissingPositive(int[] nums) {
+        int[] filteredNums = Arrays.stream(nums).filter(n -> n > 0).toArray();
+        
+        Arrays.sort(filteredNums);
+        
+        int target = 1;
+        for (int n : filteredNums) {
+            if (n == target) {
+                target++;
+            } else if (n > target) {
+                return target;
+            }
+        }
+        
+        return target;        
+    }
+}
